8005e7870e77cb84f88e8367e34f2447d70a4af5
Roger Pender <rdpender3@gmail.com>
2023-06-13 09:55:34 -0700

    starting project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+// import breadData from './data';
+
+function Bread() {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1620921568790-c1cf8984624c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80"
+        alt="Brioche Bread"
+      />
+      <h2>Brioche</h2>
+      <p>Light and slighly puffy bread</p>
+      <p>$18</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Bread />
+      <Bread />
+      <Bread />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const breadData = [
+  {
+    name: 'Sour Dough',
+    ingredients: 'A bread made by the fermentation of dough to give a sour taste',
+    price: 8,
+    photoName: 'https://images.unsplash.com/photo-1585478259715-876a6a81fc08?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80',
+    soldOut: false,
+  },
+  {
+    name: 'Whole Wheat Loaf',
+    ingredients: 'A loaf of bread made from whole wheat flour',
+    price: 4,
+    photoName: 'https://images.unsplash.com/photo-1565181917578-a87c12e04ff7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1836&q=80',
+    soldOut: false,
+  },
+  {
+    name: 'Foccacia',
+    ingredients: 'Oven backed bread with vegetable toppings',
+    price: 10,
+    photoName: 'https://images.unsplash.com/photo-1636367989765-db1ebdae1408?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80',
+    soldOut: false,
+  },
+  {
+    name: 'Fougasse',
+    ingredients: 'A bread slashed into a pattern resembling a head of wheat',
+    price: 12,
+    photoName: 'https://media.istockphoto.com/id/509560598/photo/fresh-baked-fougasse-traditional-french-bread.jpg?s=612x612&w=0&k=20&c=80xtT06CPyD_2hu3BMUY-mEJzSdEtzZZTpJG11YbcJY=',
+    soldOut: false,
+  },
+  {
+    name: 'Baguette',
+    ingredients: 'Long, rolled bread perfect for sandwiches',
+    price: 10,
+    photoName: 'https://images.unsplash.com/photo-1608198093039-81259e1b10e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1546&q=80',
+    soldOut: true,
+  },
+  {
+    name: 'Brioche',
+    ingredients: 'Light and slighly puffy bread',
+    price: 18,
+    photoName: 'https://images.unsplash.com/photo-1620921568790-c1cf8984624c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80',
+    soldOut: false,
+  },
+];
