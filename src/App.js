a7e4ebc0240f085d0a7a88a985fcd4c206056742
Roger Pender <rdpender3@gmail.com>
2023-06-15 09:55:36 -0700

    adding styles from stylesheet

--- src/App.js
+++ src/App.js
@@ -3,40 +3,57 @@ import './App.css';
 
 function Bread() {
   return (
-    <div>
+    <div className="bread">
       <img
         src="https://images.unsplash.com/photo-1620921568790-c1cf8984624c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80"
         alt="Brioche Bread"
       />
-      <h2>Brioche</h2>
-      <p>Light and slighly puffy bread</p>
-      <p>$18</p>
+      <div>
+        <h3>Brioche</h3>
+        <p>Light and slighly puffy bread</p>
+        <p>$18</p>
+      </div>
     </div>
   );
 }
 
 function Header() {
-  return <h1>Quick React Baking Co.</h1>;
+  return (
+    <header className="header">
+      <h1>Quick Rise Baking Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Bread />
       <Bread />
       <Bread />
-    </div>
+    </main>
   );
 }
 
 function Footer() {
-  return <footer>Right now it is {new Date().toLocaleTimeString()}. We're currently open!</footer>;
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closedHour = 22;
+  const isOpen = hour >= openHour && hour <= closedHour;
+
+  // if (isOpen) {
+  //   console.log("We're currently open!");
+  // } else {
+  //   console.log("We're currently closed. See you tomorrow!");
+  // }
+
+  return <footer className="footer">Right now it is {new Date().toLocaleTimeString()}. We're currently open!</footer>;
 }
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
