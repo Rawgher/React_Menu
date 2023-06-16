d0df2549acfbc4a61ad8cb011ecd730a7da927bd
Roger Pender <rdpender3@gmail.com>
2023-06-16 10:00:34 -0700

    finished basic project

--- src/components/Bread/Bread.js
+++ src/components/Bread/Bread.js
@@ -1,15 +1,17 @@
 import './bread.css';
 
 const Bread = ({ breadObj }) => {
-  const { img, name, description, price } = breadObj;
+  const { img, name, description, price, soldOut } = breadObj;
+
+  // if (soldOut) return null;
 
   return (
-    <li className="bread">
+    <li className={`bread ${soldOut ? 'sold-out' : ''}`}>
       <img src={img} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{description}</p>
-        <span>${price}</span>
+        <span>{soldOut ? 'Sold Out' : <>${price}</>}</span>
       </div>
     </li>
   );
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,18 +4,20 @@ import './menu.css';
 
 function Menu() {
   const breads = breadData;
-  // const breads = [];
   const breadLength = breads.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
       {breadLength > 0 ? (
-        <ul className="breads">
-          {breads.map((bread) => (
-            <Bread breadObj={bread} key={bread.name} />
-          ))}
-        </ul>
+        <>
+          <p>Elevate your baking experience with our delectable treats that rise in record time!</p>
+          <ul className="breads">
+            {breads.map((bread) => (
+              <Bread breadObj={bread} key={bread.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're currently out of stock of our breads. Please come back later to see what we've made!</p>
       )}
